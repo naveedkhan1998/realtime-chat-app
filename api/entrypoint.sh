b3c7f290ec74f297b3d09dda70594d017e91965d
nkhan364 <naveedkhan13041998@gmail.com>
2024-11-11 10:19:49 -0500

    backend setup

---
+++ api/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Run migrations
+echo "Applying database migrations..."
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+# Collect static files (if needed)
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
+
+# Start the server with Uvicorn ASGI workers
+echo "Starting Django ASGI server..."
+# uvicorn
+# exec gunicorn config.asgi:application --bind 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker
+# daphne
+python3 manage.py runserver 0.0.0.0:8000
